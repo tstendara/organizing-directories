efac122bebaf0152618c9b576dc357fa62957d81
Tyler <tstendara@gmail.com>
2020-05-31 03:04:01 -0500

    fixing shit

--- makingFolder.sh
+++ makingFolder.sh
@@ -8,7 +8,6 @@ mkdir icons Pictures fonts Movies
 
 echo 'testing'
 npm test
-echo '^ Should have any output. If it does, then assure that you're in the right directory'
 EOF
 echo 'Moving files to root'
 mv * $(echo $TRGT | tr -d '\r')

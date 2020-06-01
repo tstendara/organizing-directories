52f810b3268196efd2d949e876cbbd8755d5dde8
Tyler <tstendara@gmail.com>
2020-05-31 23:07:17 -0500

    download homebrew and install node

--- makingFolder.sh
+++ makingFolder.sh
@@ -1,7 +1,15 @@
-TRGT='/Users/USERNAME'
+echo 'installing homebrew'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo 'exporting path'
+export PATH="/usr/local/bin:$PATH"
+
+echo 'installing node and npm'
+brew install node
 
 echo 'installing dependencies'
 npm i
+
 cat > settingUp.sh <<-"EOF"
 echo 'Creating directories'
 mkdir icons Pictures fonts Movies
@@ -9,5 +17,7 @@ mkdir icons Pictures fonts Movies
 echo 'testing'
 npm test
 EOF
+
 echo 'Moving files to root'
-mv * $(echo $TRGT | tr -d '\r')
+mv * ../../
+# assuming that the directory is in a folder on the desktop
